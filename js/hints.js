1b00f43a76363f0489783d681f58e1d15739217c
Serhan Uygur <serhan.uygur@gmail.com>
2017-06-27 17:04:11 +0200

    jQuery Widget the way to do privateVars
    This is it!

--- js/hints.js
+++ js/hints.js
@@ -1,17 +1,19 @@
 $.widget("su.hint", {
     options: {
         hintFor: "common",
-        opened:  true
+        opened: true
     },
-
+    privateOptions: {},
+    _privateVariables: function() {
+        this.privateOptions.inputField = $('#' + this.options.hintFor).parent();
+        this.privateOptions.that = this;
+    },
     _create: function() {
         this._privateVariables();
-        this.element.attr('id', hintID);
+        this.element.attr('id', "hint-" + this.options.hintFor);
 
     },
 
-    _privateVariables: function () {
-        hintID = "hint-" + this.options.hintFor;
     },
 
 
