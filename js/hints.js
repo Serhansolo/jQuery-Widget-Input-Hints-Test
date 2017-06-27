2287c1dc65fa2f0ef0ad2269013c427ef282f838
Serhan Uygur <serhan.uygur@gmail.com>
2017-06-27 17:04:52 +0200

    Add open and close buttons by Widget

--- js/hints.js
+++ js/hints.js
@@ -12,9 +12,35 @@ $.widget("su.hint", {
         this._privateVariables();
         this.element.attr('id', "hint-" + this.options.hintFor);
 
+        this._addOpenButton();
+        this._addCloseButton();
     },
 
+    close: function() {
+        this.element.addClass('close');
     },
 
+    open: function() {
+        this.element.removeClass('close');
+    },
+
+    _addOpenButton: function() {
+        var button = $('<button class="open-hint for-hint-' + this.options.hintFor + '" type="button">?</button>');
+        this.privateOptions.inputField.append(button);
+        var that = this;
+        button.on("click", function() {
+            that.open();
+        })
+    },
+
+    _addCloseButton: function() {
+        var button = $('<button class="close-hint for-hint-' + this.options.hintFor + '" type="button">X</button>');
+        this.element.append(button);
+        var that = this;
+        button.on("click", function() {
+                that.close();
+            }
+        )
+    }
 
 });
