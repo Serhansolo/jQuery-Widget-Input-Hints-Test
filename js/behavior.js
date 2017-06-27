f3dfae9225a5dc09235268639cf5f62cad62b7bd
Serhan Uygur <serhan.uygur@gmail.com>
2017-06-27 18:33:11 +0200

    jQuery Widget Close Open hints... done!
    WOOHOO!

--- js/behavior.js
+++ js/behavior.js
@@ -1,6 +1,13 @@
-$(document).ready( function() {
+$(document).ready(function() {
     $('.hint').each(function() {
         var parentID = $(this).prev().find('input').attr('id');
         $(this).hint({hintFor: parentID});
     });
+
+    $('.hide-all').on('click', function() {
+        $('.hint').each(function() {
+            $(this).hint('close');
+        })
+
+    })
 });
--- js/hints.js
+++ js/hints.js
@@ -1,32 +1,45 @@
 $.widget("su.hint", {
     options: {
         hintFor: "common",
-        opened: true
+        opened: false
     },
-    privateOptions: {},
-    _privateVariables: function() {
-        this.privateOptions.inputField = $('#' + this.options.hintFor).parent();
-        this.privateOptions.that = this;
-    },
+
+    // Initialize our widget functionality
     _create: function() {
-        this._privateVariables();
         this.element.attr('id', "hint-" + this.options.hintFor);
 
         this._addOpenButton();
         this._addCloseButton();
+
+        if (this.options.opened) {
+            this.open();
+        } else {
+            this.close();
+        }
     },
 
+    // Public function needed to close the hint and do the right stuff so open other parts.
     close: function() {
         this.element.addClass('close');
+        var inputField = $('#' + this.options.hintFor);
+        inputField.next().removeClass('hide');
     },
 
+    // Public function needed to open the hint and do the right stuff so close other parts.
     open: function() {
         this.element.removeClass('close');
+        var inputField = $('#' + this.options.hintFor);
+        inputField.next().addClass('hide');
+
     },
 
     _addOpenButton: function() {
+        // Add the open button
         var button = $('<button class="open-hint for-hint-' + this.options.hintFor + '" type="button">?</button>');
-        this.privateOptions.inputField.append(button);
+        var inputField = $('#' + this.options.hintFor);
+        inputField.parent().append(button);
+
+        // Give this open button an on click open functionality
         var that = this;
         button.on("click", function() {
             that.open();
@@ -34,13 +47,15 @@ $.widget("su.hint", {
     },
 
     _addCloseButton: function() {
+        // Add the close button
         var button = $('<button class="close-hint for-hint-' + this.options.hintFor + '" type="button">X</button>');
         this.element.append(button);
+
+        // Give this close button an on click close functionality
         var that = this;
         button.on("click", function() {
-                that.close();
-            }
-        )
+            that.close();
+        })
     }
 
 });
